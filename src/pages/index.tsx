db5b787152f32ebfe8876e78d72c32b34d41e37e
dangoo <daniel.gooss@sinnerschrader.com>
2020-12-18 23:34:58 +0100

    feat(font): add graphik

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { basePath } from "../../next.config";
 
 export default function Home() {
   return (
@@ -57,7 +58,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src={`${basePath}/vercel.svg`} alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
