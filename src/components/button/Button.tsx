9dce040af4c5310464b7e9a3778adffa65e0788c
dangoo <daniel.gooss@sinnerschrader.com>
2021-01-07 19:34:58 +0100

    feat(button): refactor

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,33 +1,20 @@
+import { forwardRef } from "react";
 import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.types";
 
-export function Button(
-  props: React.PropsWithChildren<
-    {
-      as: "a";
-    } & Omit<ButtonProps, "type">
-  >
-): JSX.Element;
-
-export function Button(
-  props: React.PropsWithChildren<
-    {
-      as?: "button";
-    } &  Omit<ButtonProps, "as" | "href">
-  >
-): JSX.Element;
-
-export function Button({
-  as,
-  children,
-  onClick = () => {},
-  ...restProps
-}: React.PropsWithChildren<ButtonProps>): JSX.Element {
+export const Button = forwardRef<any, ButtonProps>(function Button(
+  { as, children, ...restProps },
+  ref
+) {
   const Component = as || "button";
 
   return (
-    <Component className={styles.default} onClick={onClick} {...restProps}>
+    <Component
+      ref={ref}
+      className={styles.default}
+      {...restProps}
+    >
       {children}
     </Component>
   );
-}
+});
--- src/components/button/Button.types.ts
+++ src/components/button/Button.types.ts
@@ -1,6 +1,21 @@
-export interface ButtonProps {
+import { PropsWithChildren } from "react";
+
+export interface ButtonBaseProps {
   as?: "a" | "button";
   href?: HTMLAnchorElement["href"];
+  style?: React.CSSProperties
   type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
   onClick?(event?: React.MouseEvent): void;
 }
+
+interface ButtonAnchorProps
+  extends PropsWithChildren<Omit<ButtonBaseProps, "type">> {
+  as: "a";
+}
+
+interface ButtonButtonProps
+  extends PropsWithChildren<Omit<ButtonBaseProps, "as" | "href">> {
+  as?: "button";
+}
+
+export type ButtonProps = ButtonAnchorProps | ButtonButtonProps;
